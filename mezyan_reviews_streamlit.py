153fd9a7a5ac78dc40fddca1d37e34721280020f
Jad-Shahine <136175739+Jad-Shahine@users.noreply.github.com>
2024-04-21 23:07:37 +0300

    Update mezyan_reviews_streamlit.py

--- mezyan_reviews_streamlit.py
+++ mezyan_reviews_streamlit.py
@@ -17,6 +17,12 @@ from bertopic import BERTopic
 from umap import UMAP
 import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.exceptions import SessionNotCreatedException
 from bs4 import BeautifulSoup
 import csv
 import time
